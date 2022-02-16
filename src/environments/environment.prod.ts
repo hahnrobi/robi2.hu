ee3522ab93acff37fead8e6aa39cec0d90fe823e
Hahn Robi <robitwo.r2@gmail.com>
2022-02-16 13:52:56 +0100

    Typo fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     space: "ylttp6j3w9z2",
     accessToken: "rKq4WIPbYVtxQH1pCxi-_5WZoqPYyA4sykdKR8TJCbQ"
   },
-  blogApiUrl: "//r2blogapi.carbon.iva.hu/",
+  blogApiUrl: "//r2blogapi.carbon.iva.hu",
   uptimes: {
     apiUrl: "//robi2.hu/hetrix.php"
   }

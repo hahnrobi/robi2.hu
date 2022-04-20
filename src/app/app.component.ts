398637b2857d84d42e8fce0d2b074a1fb2e77b09
Hahn Robi <robitwo.r2@gmail.com>
2022-04-20 20:00:19 +0200

    No JS loader before Angular render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { NavigationStart, Router } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { faFacebookF, faYoutube, faTwitter, faFlickr, faSpotify, faGithub, faDiscord } from '@fortawesome/free-brands-svg-icons';
 import { faAddressCard, faBars, faCode, faComments, faHeartbeat, faLink, faPlus, faToolbox } from '@fortawesome/free-solid-svg-icons';
 import { blurer} from './route-animations';
 
 @Component({
-  selector: 'app-root',
+  selector: 'body[robi2]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
@@ -16,7 +16,7 @@ export class AppComponent {
   title = 'robi2';
   isExpanded = false;
 
-  constructor(private router:Router) {
+  constructor(private router:Router, private renderer:Renderer2) {
     router.events.subscribe(val => {if(val instanceof NavigationStart) this.isExpanded = false});
   }
   onRouterActivate(event) {

da69dfb3effed9ca725c4eaa45be236c07975306
Hahn Robi <robitwo.r2@gmail.com>
2022-06-09 17:00:21 +0200

    OnPush changeDetection

--- src/app/uptime/components/uptime-service/uptime-service.component.ts
+++ src/app/uptime/components/uptime-service/uptime-service.component.ts
@@ -1,11 +1,12 @@
 import { UptimeSite } from '../../model/site';
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-uptime-service',
   templateUrl: './uptime-service.component.html',
-  styleUrls: ['./uptime-service.component.scss']
+  styleUrls: ['./uptime-service.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UptimeServiceComponent {
   @Input() site:UptimeSite

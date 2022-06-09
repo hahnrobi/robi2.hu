80c1d81730d64563650ec7ddd946d47cbf83f14c
Hahn Robi <robitwo.r2@gmail.com>
2022-06-09 16:51:41 +0200

    Reactive approach

--- src/app/uptime/uptime.service.ts
+++ src/app/uptime/uptime.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { interval, ReplaySubject, timer, Observable } from 'rxjs';
-import { takeUntil, takeWhile } from 'rxjs/operators';
+import { interval, ReplaySubject, timer, Observable, Subject, BehaviorSubject } from 'rxjs';
+import { takeUntil, takeWhile, switchMap, share } from 'rxjs/operators';
 import { environment } from './../../environments/environment';
 import { UptimeGroup } from './model/group';
 
@@ -9,46 +9,16 @@ import { UptimeGroup } from './model/group';
   providedIn: 'root'
 })
 export class UptimeService {
-
-  private uptime$: ReplaySubject<UptimeGroup[]>;
-  private timerStarted = false;
-  private lastUpdate;
-
-  constructor(private http:HttpClient) {
-    this.uptime$ = new ReplaySubject<UptimeGroup[]>();
-  }
-  private timerLogic(): ReplaySubject<UptimeGroup[]> {
-    this.timerStarted = true;
-    this.updateUptimeData();
-    let rxInterval = interval(30000);
-    const intervalSub = rxInterval.subscribe(tick => {
-      if(this.uptime$.observers.length === 0) {
-        intervalSub.unsubscribe();
-        this.timerStarted = false;
-      }else {
-        this.updateUptimeData();
-      }
-    })
-    return this.uptime$;
-  }
-
-  private makeRequest():Observable<UptimeGroup[]> {
-      return this.http.get<UptimeGroup[]>(environment.uptimes.apiUrl + "?" + new Date().getTime());
-  }
+  constructor(private http:HttpClient) {}
   
-  updateUptimeData() {
-    if(!this.lastUpdate || this.lastUpdate + 1000 < new Date().getTime()) {
-      this.lastUpdate = new Date().getTime();
-      this.makeRequest().subscribe(d => this.uptime$.next(d));
-      console.log("Uptimes updated.");
-    }
-    
-  }
+  private uptime$ = timer(0, 30000).pipe(
+    switchMap(() => {
+      return this.http.get<UptimeGroup[]>(environment.uptimes.apiUrl + "?" + new Date().getTime())
+    }),
+    share()
+  );
 
-  getUptime(): ReplaySubject<UptimeGroup[]> {
-    if(this.timerStarted) {
-      return this.uptime$;
-    }
-    return this.timerLogic();
+  public getUptime() {
+    return this.uptime$;
   }
 }
